e6f01d480adc193c6623e0efd9b497cc7c35fb8b
AlonMoreira <alissonlim13@gmail.com>
2025-05-13 19:36:36 -0300

    Criação de menu de controle do programa

---
+++ src/funcao.py
@@ -0,0 +1,9 @@
+def menu():
+    print('-=-'*15)
+    print(' '*9, 'Cadastro de Reclamações')
+    print('-=-'*15)
+    print('Cadastrar reclamação   [ 1 ]')
+    print('Visualizar reclamações [ 2 ]')
+    print('Sair do programa       [ 3 ]')
+    opcao = input('Digite a sua opção: ')
+    return opcao
