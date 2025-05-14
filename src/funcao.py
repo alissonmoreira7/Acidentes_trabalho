1355a59503d220ea4fd3ef4e021663092cd7e898
AlonMoreira <alissonlim13@gmail.com>
2025-05-14 12:28:56 -0300

    Deixando o código principal mais ccurto

--- src/funcao.py
+++ src/funcao.py
@@ -1,3 +1,4 @@
+#Menu de controle do programa
 def menu():
     print('-=-'*15)
     print(' '*9, 'Cadastro de Reclamações')
@@ -5,5 +6,4 @@ def menu():
     print('Cadastrar reclamação   [ 1 ]')
     print('Visualizar reclamações [ 2 ]')
     print('Sair do programa       [ 3 ]')
-    opcao = input('Digite a sua opção: ')
-    return opcao
+    
