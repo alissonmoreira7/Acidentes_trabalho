2dc92d5c6687023d57f3437d3e29676c9c47e752
AlonMoreira <alissonlim13@gmail.com>
2025-05-26 20:51:24 -0300

    Organizando funções em módulo separado do código principal.

--- src/controle.py
+++ src/controle.py
@@ -1,67 +1,11 @@
+#Anotações
+#Adicionar um banco de dados ao invés de usar um arquivo csv.
+#Utilizar Programação Orientada a Objetos para modelar as reclamações.
+
 import csv
 import os
 from datetime import datetime
-from funcao import menu
+from funcao import visualizar_reclamacoes, cadastrar_reclamacao, obter_ultimo_id, menu
 
-while True:
+ARQUIVO = "../data/reclamacoes.csv"
 
-    menu()
-    while True:
-
-        opcao = int(input('Digite a sua opção: '))
-
-        if opcao not in (1, 2, 3):
-             print('Erro! Tente digitar um valor entre 1 e 3.')
-             continue
-        else:
-             break
-
-    # Cadastrar mais reclamações
-    if opcao == 1:
-                count = 0
-                file_path = "../data/reclamacoes.csv"
-                
-                with open(file_path, 'a', newline='') as r2:
-                    df_csv = csv.writer(r2)
-                    
-                    # Cabeçalho da tabela
-                    if os.stat(file_path).st_size == 0:
-                        df_csv.writerow(('id', 'tipo', 'local', 'data')) #Cabeçalho
-                    
-                    while True:
-                        # Itens da tabela
-                        tipo = input('Qual é o tipo da sua reclamação? ')
-                        local = input('Qual foi o local da ocorrência? ')
-
-                        try:
-                            date = input('Qual foi a data do ocorrido? (DD/MM/AAAA): ')
-                            datetime.strptime(date, "%d/%m/%Y")
-                            
-                        except ValueError:
-                            print("Data inválida! Use o formato DD/MM/AAAA.")
-                            continue
-
-                        # Verificação do preenchimento correto dos campos
-                        if tipo and local and date:
-                            count += 1
-                            df_csv.writerow((count, tipo, local, date))
-
-                        else:
-                            print("Todos os campos são obrigatórios!")
-
-                        escolha = input('Você deseja cadastrar outra reclamação? [S/N]').lower()
-                        if escolha != 's':
-                            break
-
-
-            #Visualizar os dados das reclamações
-    elif opcao == 2:
-                with open("../data/reclamacoes.csv", 'r') as r2:
-                    df_csv = csv.reader(r2, delimiter=',')
-
-                    for linha in df_csv:
-                        print(linha)
-
-    elif opcao == 3:
-        print("Saindo do programa...")
-        break
--- src/funcao.py
+++ src/funcao.py
@@ -1,3 +1,7 @@
+import csv
+import os
+import datetime
+from controle import ARQUIVO
 
 #Menu de controle do programa
 def menu():
@@ -7,3 +11,52 @@ def menu():
     print('Cadastrar reclamação   [ 1 ]')
     print('Visualizar reclamações [ 2 ]')
     print('Sair do programa       [ 3 ]')
+
+#Obtendo o último id
+def obter_ultimo_id():
+    if not os.path.exists(ARQUIVO) or os.stat(ARQUIVO).st_size == 0: #Verificando a existência ou se tem algo dentro do arquivo.
+        return 0
+    with open(ARQUIVO, 'r') as file:
+        linhas = list(csv.reader(file)) 
+        if len(linhas) <= 1: #Se linhas tem uma ou nada de conteúdo quer dizer que ou tem o cabeçalho ou nada.
+             return 0
+        return int(linhas[-1][0])
+
+#Cadastar reclamação
+def cadastrar_reclamacao():
+    ultimo_id = obter_ultimo_id()
+    with open(ARQUIVO,'a', newline='') as arquivo:
+        dados_arquivo = csv.writer(arquivo)
+
+        if os.stats(ARQUIVO).st_size == 0: #Se está vazio, insira o cabeçalho.
+            dados_arquivo.writerow(['id', 'tipo', 'local', 'data'])
+
+        while True:
+            tipo = input('Qual é o tipo da sua reclamação? ').strip()
+            local = input('Qual foi o local da ocorrência? ').strip()
+            date = input('Qual foi a data do ocorrido? (DD/MM/AAAA): ').strip()
+
+            #Válidando a entrada correta da data e formatada.
+            try:
+                datetime.strptime(date, "%d/%m/%Y")
+            except ValueError:
+                print('Data inválida! Use o formato DD/MM/AAAA.')
+                continue 
+
+            if tipo and local and date:
+                ultimo_id += 1
+                dados_arquivo.writerow([ultimo_id, tipo, local, date])
+                print('Reclamação cadastrada com sucesso!!!')
+            else:
+                print('Todos os campos são necessários!!!')
+
+#Visualizando as reclamações do arquivo .CSV
+def visualizar_reclamacoes():
+    if not os.path.exists(ARQUIVO) or os.stat(ARQUIVO).st_size == 0:
+        print('Atenção! Nenhuma reclamação cadastradas.')
+        return
+    
+    with open(ARQUIVO, 'r') as arquivo:
+        leitor = csv.reader(arquivo)
+        for linha in leitor:
+            print(f"{linha[0]:<4} | {linha[1]:<20} | {linha[2]:<20} | {linha[3]}")
